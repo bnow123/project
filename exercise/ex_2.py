8805c9676c548c76a3a137ed95b97b7908da090a
Beata Nowicka <73627@student.ue.poznan.pl>
2023-03-27 12:26:11 +0200

    Dodanie trapezu

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -22,6 +22,17 @@ print("Obwod prostokata wynosi " + str(obwod) + ",a pole prostokata wynosi " + s
 r = 5
 pi = math.pi
 obwod = 2 * pi * r
-pole = pi * r * r
+pole = pi * r * 2
 
 print("Obwod kola wynosi "+ str(obwod) + ",a pole kola wynosi " + str(pole) + ".")
+
+# trapez
+
+a = 3
+b = 7
+c = 5
+d = 6
+h = 3
+obwod = a + b + c + d
+pole = ((a + b) * h)/2
+print("Obwod trapezu wynosi "+ str(obwod) + ",a pole trapezu wynosi " + str(pole) + ".")
