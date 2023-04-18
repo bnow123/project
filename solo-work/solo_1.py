8310c0c0b8a992e6ab43bbed93666b2866fb5311
Beata Nowicka <73627@student.ue.poznan.pl>
2023-04-18 21:04:46 +0200

    zad 1.6

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -20,3 +20,9 @@ wynik = liczba ** potega
 # oczekiwany rezultat:
 # Wynik wynosi: 125
 print("Wynik wynosi: {}".format(wynik))
+# zadanie 1.6
+ciag_znakow = "edbw(hdakqas(skqskahb))adwndwb(wgwidn()dsqwhjdw)"
+liczba_nawiasow_otwierajacych = ciag_znakow.count('(')
+# oczekiwany rezultat:
+# Liczba nawiasow otwierajacych wynosi: 4
+print("Liczba nawiasow otwierajacych wynosi: {}".format(liczba_nawiasow_otwierajacych))
