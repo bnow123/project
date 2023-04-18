e48da0bd26adfab73c36bb97e97d1a0b999865bf
Beata Nowicka <73627@student.ue.poznan.pl>
2023-04-18 19:49:10 +0200

    zad 1.1

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -1,11 +1,3 @@
-# zadanie 1.1
 hello = "Hello"
 student = "Beata"
-print(str(print(hello + " " + student)))
-<<<<<<< HEAD
-
-=======
-# zadanie 1. 2
-student = input("Wpisz swoje imię:")
-print("Hello" + " " + student)
->>>>>>> solo-python-work
+str(print(hello + " " + student))
