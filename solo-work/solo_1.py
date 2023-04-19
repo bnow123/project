8832355c34fc67748dd999c52a205966e3f87d53
Beata Nowicka <73627@student.ue.poznan.pl>
2023-04-19 12:59:29 +0200

    zad 1.10

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -51,3 +51,43 @@ for i in range(len(students)):
 students = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
 liczba_n = len([element for element in students if "N" in element])
 print("Liczba osob, ktorych nazwiska zaczynaja sie na N: ", liczba_n)
+# zadanie 1.10
+
+
+def wektory(list_a, list_b):
+    result = []
+    for item_a, item_b in zip(list_a, list_b):
+        result.append(item_b - item_a)
+    return result
+
+
+def rownosc_wektorow(list_a, list_b):
+    result = True
+    for item_a, item_b in zip(list_a, list_b):
+        if item_a != item_b:
+            result = False
+    return result
+
+
+wykres_1 = [[2, 4], [4, 4], [6, 4]]
+wykres_2 = [[2, 3], [4, 4], [6, 5]]
+wykres_3 = [[2, 3], [4, 3], [5, 4]]
+
+wykres_1_funkcja_liniowa = rownosc_wektorow(wektory(wykres_1[0], wykres_1[1]), wektory(wykres_1[1], wykres_1[2]))
+wykres_2_funkcja_liniowa = rownosc_wektorow(wektory(wykres_2[0], wykres_2[1]), wektory(wykres_2[1], wykres_2[2]))
+wykres_3_funkcja_liniowa = rownosc_wektorow(wektory(wykres_3[0], wykres_3[1]), wektory(wykres_3[1], wykres_3[2]))
+
+if wykres_1_funkcja_liniowa:
+    print("Dla punktow w wykres_1 mozna wyznaczyc funkcje liniowa.")
+else:
+    print("Dla punktow w wykres_1 nie mozna wyznaczyc funkcji liniowej.")
+
+if wykres_2_funkcja_liniowa:
+    print("Dla punktow w wykres_2 mozna wyznaczyc funkcje liniowa.")
+else:
+    print("Dla punktow w wykres_2 nie mozna wyznaczyc funkcji liniowej.")
+
+if wykres_3_funkcja_liniowa:
+    print("Dla punktow w wykres_3 mozna wyznaczyc funkcje liniowa.")
+else:
+    print("Dla punktow w wykres_3 nie mozna wyznaczyc funkcji liniowej.")
