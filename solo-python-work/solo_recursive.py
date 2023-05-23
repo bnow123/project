6b30ae209f6e83b751f227ad905a82df4ad6436d
Beata Nowicka <73627@student.ue.poznan.pl>
2023-05-23 18:09:42 +0200

    silnia funkcja

--- solo-python-work/solo_recursive.py
+++ solo-python-work/solo_recursive.py
@@ -13,4 +13,21 @@ def sum_list(l):
         l.pop(0)
         return data + sum_list(l)
 
+# FUNKCJA SILNIA
+# function n!
+# function silnia -> if n == 0 return 1
+# because 0! == 1 for n=/=0 n * silnia (n-1)
+# example why: n == 5 so silnia(5) = 5 * 4 * 3 * 2 * 1
+# we can write it in simple way because silnia(4) = 4 * 3 * 2 * 1, so silnia(5) = 5 * silnia(4)
+# so by defining a function "silnia" we check for each n if its 0 or not,
+# and if not we count n * silnia(n-1)
+# silnia(n-1) works the same way if n-1 =/= 0 we count (n-1)* silnia(n-2) etc.
+
+
+def silnia(n):
+    if n == 0:
+        return 1
+    else:
+        return n * silnia(n - 1)
+
 
