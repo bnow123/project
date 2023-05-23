5d50af12088180e8ecbb544f3db46b2932423b04
Beata Nowicka <73627@student.ue.poznan.pl>
2023-05-23 18:10:52 +0200

    sudoku

--- solo-python-work/solo_recursive.py
+++ solo-python-work/solo_recursive.py
@@ -86,4 +86,121 @@ def fibonacci_recursive(n):
         sequence.append(next_number)
         return sequence
 
+# sudoku
+# wypełnienie diagramu 9 × 9 w taki sposób, aby w każdym wierszu,
+# w każdej kolumnie i w każdym z dziewięciu kwadratów
+# 3 × 3 znalazło się po jednej cyfrze od 1 do 9.
+# dla każdej komórki przypisany jest numer wiersza i numer kolumny ( row, col)
+# sudoku oznaczamy jako tablicę, w której niektóre pola są wypełnione dlatego chcemy,
+# żeby nasza funkcja szukała pustą komórkę w tablicy, ponieważ to te pola wypełniamy
+# jeśli nie ma już pustych komórek to zwraca wartość "Done", co znaczy, że Sudoku zostało rozwiązane
+# aby odnaleźć pusta komórkę definiujemy nową funkcje find_empty_cell
+# jak już znajdzie pustą komórkę określoną współrzędnymi row, col to sprawdza za pomocą funkcji is_valid,
+# która sprawdza poprawność wstawienia liczby do danej komórki
+# Przyjmuje ona cztery argumenty:
+# `board` (plansza Sudoku),
+# `row` (indeks wiersza),
+# `col` (indeks kolumny)
+# oraz `num` (liczba do sprawdzenia).
+# funkcja ta sprawdzaa czy dana liczba num może być w danej komórce sprawdza czy
+# nie wystąpiła już w tym samym wierszu(z wyjątkiem tej aktualnej), kolumny, kwadratu 3x3
+# Funkcja `is_valid` sprawdza, czy liczba `num` może zostać umieszczona w danej komórce
+# jeśli Tak to zwraca True jeśli nie to zwraca False
+# oznaczenie board[row][col] =0 -> przypisanie 0 do danej komórki oznacza, że komórka jest pusta
+# komentarze do danych funkcji, tłumaczące ich działanie są poniżej
+
+
+def solve_sudoku(board):
+    # Sprawdź, czy plansza jest już rozwiązana
+    if is_sudoku_solved(board):
+        return "Done"
+
+    # Znajdź następne wolne pole do wypełnienia
+    row, col = find_empty_cell(board)
+
+    # Próbuje wypełnić pole liczbami od 1 do 9
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            # Jeśli num może być wstawione, wypełnij pole
+            board[row][col] = num
+
+            # Kontynuuj rekurencyjnie, jeśli rozwiązanie jest możliwe
+            if solve_sudoku(board) == "Done":
+                return "Done"
+
+            # Jeśli rozwiązanie jest niewłaściwe, cofnij i spróbuj inną liczbę
+            board[row][col] = 0
+
+    # Jeśli żadna liczba nie pasuje, zwróć False
+    return False
+
+
+def is_sudoku_solved(board):
+    # Sprawdź, czy plansza jest już rozwiązana poprawnie
+    # Sprawdzamy, czy w każdym wierszu, kolumnie i bloku 3x3 nie ma powtórzeń
+    # oraz czy nie ma żadnych pustych pól (wartości 0)
+
+    # Sprawdź wiersze
+    for row in board:
+        if sorted(row) != list(range(1, 10)):
+            return False
+
+    # Sprawdź kolumny
+    for col in range(9):
+        column_values = [board[row][col] for row in range(9)]
+        if sorted(column_values) != list(range(1, 10)):
+            return False
+
+    # Sprawdź bloki 3x3
+    for block_row in range(0, 9, 3):
+        for block_col in range(0, 9, 3):
+            block_values = []
+            for row in range(block_row, block_row + 3):
+                for col in range(block_col, block_col + 3):
+                    block_values.append(board[row][col])
+            if sorted(block_values) != list(range(1, 10)):
+                return False
+
+    return True
+
+
+def is_valid(board, row, col, num):
+    # Sprawdź, czy wstawienie liczby num do pola (row, col) jest prawidłowe
+    # Sprawdzamy, czy num nie występuje już w tym samym wierszu, kolumnie i bloku 3x3
+
+    # Sprawdź wiersz
+    if num in board[row]:
+        return False
+
+    # Sprawdź kolumnę
+    for r in range(9):
+        if board[r][col] == num:
+            return False
+
+    # Sprawdź blok 3x3
+    block_row = (row // 3) * 3
+    block_col = (col // 3) * 3
+    for r in range(block_row, block_row + 3):
+        for c in range(block_col, block_col + 3):
+            if board[r][c] == num:
+                return False
+
+    return True
+
+# W funkcji `find_empty_cell`,
+# Zwrócenie wartości (-1, -1) jako wynik,
+#  informuje, że nie ma więcej wolnych pól do wypełnienia.
+# W przypadku funkcji `solve_sudoku`, jeśli otrzymamy (-1, -1) jako wynik z `find_empty_cell`,
+# oznacza to, że cała plansza została wypełniona
+
+
+def find_empty_cell(board):
+    # Znajdź pierwsze wolne pole na planszy
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                return row, col
+    return -1, -1
+
+
 
