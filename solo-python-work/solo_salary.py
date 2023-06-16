4a8a48766ee8c5e98c1c31d34533c82d1e4d0ab1
Beata Nowicka <73627@student.ue.poznan.pl>
2023-06-16 17:04:11 +0200

    calosc

--- solo-python-work/solo_salary.py
+++ solo-python-work/solo_salary.py
@@ -1,4 +1,8 @@
+import csv
+
+
 class Pracownik:
+
     def __init__(self, imie, nazwisko, wynagrodzenie_brutto):
         self.imie = imie
         self.nazwisko = nazwisko
@@ -11,3 +15,95 @@ class Pracownik:
         Wynagrodzenie_brutto: {self.wynagrodzenie_brutto}
         """
         return pracownik_info.strip()
+
+    def oblicz_wynagrodzenie_netto(self):
+        skladki = self.oblicz_skladki()
+        wynagrodzenie_netto = self.wynagrodzenie_brutto - skladki
+        return wynagrodzenie_netto
+
+    def oblicz_skladki(self):
+        skladki_zus_emerytalne = self.wynagrodzenie_brutto * 0.0976
+        skladki_zus_rentowe = self.wynagrodzenie_brutto * 0.015
+        skladki_zus_chorobowe = self.wynagrodzenie_brutto * 0.0245
+        skladki_ubezpieczenie_zdrowotne = self.wynagrodzenie_brutto * 0.09
+        skladki_fundusz_pracy = self.wynagrodzenie_brutto * 0.0245
+        skladki_fgsp = self.wynagrodzenie_brutto * 0.001
+        skladki_fep = self.wynagrodzenie_brutto * 0.015
+        skladki = (
+            skladki_zus_emerytalne +
+            skladki_zus_rentowe +
+            skladki_zus_chorobowe +
+            skladki_ubezpieczenie_zdrowotne +
+            skladki_fundusz_pracy +
+            skladki_fgsp +
+            skladki_fep
+        )
+        return skladki
+
+    def oblicz_koszty(self):
+        skladki_zus_emerytalne = self.wynagrodzenie_brutto * 0.0976
+        skladki_zus_rentowe = self.wynagrodzenie_brutto * 0.015
+        skladki_zus_chorobowe = self.wynagrodzenie_brutto * 0.0245
+        skladki_ubezpieczenie_zdrowotne = self.wynagrodzenie_brutto * 0.09
+        skladki_fundusz_pracy = self.wynagrodzenie_brutto * 0.0245
+        skladki_fgsp = self.wynagrodzenie_brutto * 0.001
+        skladki_fep = self.wynagrodzenie_brutto * 0.015
+        koszty = (
+            skladki_zus_emerytalne +
+            skladki_zus_rentowe +
+            skladki_zus_chorobowe +
+            skladki_ubezpieczenie_zdrowotne +
+            skladki_fundusz_pracy +
+            skladki_fgsp +
+            skladki_fep
+        )
+        return koszty
+
+    def oblicz_koszt_pracodawcy(self):
+        koszty = self.oblicz_koszty()
+        return self.wynagrodzenie_brutto + koszty
+
+
+def oblicz_koszt_calkowity(nazwa_pliku):
+    koszt_calkowity = 0
+
+    with open(nazwa_pliku, 'r', newline='') as plik_csv:
+        czytnik = csv.reader(plik_csv)
+        next(czytnik)  # Pominięcie nagłówka
+
+        for row in czytnik:
+            imie, nazwisko, wynagrodzenie_brutto = row
+            pracownik = Pracownik(imie, nazwisko, wynagrodzenie_brutto)
+            koszt_pracownika = pracownik.oblicz_koszty()
+            koszt_calkowity += koszt_pracownika
+
+    return koszt_calkowity
+
+
+nazwa_pliku = 'pracownicy.csv'
+koszt_calkowity = oblicz_koszt_calkowity(nazwa_pliku)
+print("Całkowity koszt pracodawcy:", koszt_calkowity)
+
+print("Informacje o pracownikach:")
+with open(nazwa_pliku, 'r', newline='') as plik_csv:
+    czytnik = csv.reader(plik_csv)
+    next(czytnik)  # Pominięcie nagłówka
+
+    for row in czytnik:
+        imie, nazwisko, wynagrodzenie_brutto = row
+        pracownik = Pracownik(imie, nazwisko, wynagrodzenie_brutto)
+        koszt_pracownika = pracownik.oblicz_koszty()
+        koszt_pracodawcy = pracownik.oblicz_koszt_pracodawcy()
+        wynagrodzenie_netto = pracownik.oblicz_wynagrodzenie_netto()
+
+        print("Pracownik:")
+        print("- Imię:", imie)
+        print("- Nazwisko:", nazwisko)
+        print("- Wynagrodzenie brutto:", wynagrodzenie_brutto)
+        print("- Koszt pracownika:", koszt_pracownika)
+        print("- Koszt pracodawcy:", koszt_pracodawcy)
+        print("- Wynagrodzenie netto:", wynagrodzenie_netto)
+        print()
+
+suma_kosztow = oblicz_koszt_calkowity(nazwa_pliku)
+print("Suma kosztów wynosi:", suma_kosztow)
